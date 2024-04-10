df1028ac3d84c19ea6973d6624d63c4de5923f6c
JuanArano17 <juanarano17@gmail.com>
2024-04-10 11:02:22 +0200

    cambiso random 2

--- DocxGenerator.py
+++ DocxGenerator.py
@@ -1,27 +1,30 @@
 from docx import Document
 from docx.shared import Pt
 from docx.enum.text import WD_PARAGRAPH_ALIGNMENT
-from docx.shared import Inches
-from EconomyAgent import getResult
-from datetime import date
+from datetime import date, datetime
+import os
 
-doc = Document()
+def generateDocx(result):
+    folder_name = 'economic_analisis_2024'
+    doc = Document()
 
-style = doc.styles['Normal']
-font = style.font
-font.name = 'Times New Roman'
-font.size = Pt(14)
-day = date.today()
+    style = doc.styles['Normal']
+    font = style.font
+    font.name = 'Times New Roman'
+    font.size = Pt(14)
+    day = date.today()
+    full_day = datetime.today()
 
-title = doc.add_paragraph(f'\nAnálisis de la Economía Argentina dia {day}')
-title.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
-title_run = title.runs[0]
-title_run.bold = True
-title_run.font.size = Pt(24)
+    title = doc.add_paragraph(f'\nAnálisis de la Economía Argentina dia {day}')
+    title.alignment = WD_PARAGRAPH_ALIGNMENT.CENTER
+    title_run = title.runs[0]
+    title_run.bold = True
+    title_run.font.size = Pt(24)
 
-doc.add_paragraph('\n\n')
-content = doc.add_paragraph(getResult())
-content.alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
+    doc.add_paragraph('\n\n')
+    content = doc.add_paragraph(result)
+    content.alignment = WD_PARAGRAPH_ALIGNMENT.JUSTIFY
 
-# Guardar el documento
-doc.save('Analisis_Economia_Argentina.docx')
+    # Guardar el documento
+    doc_path = os.path.join(folder_name, f'Analisis_Economia_Argentina_{full_day}.docx')
+    doc.save(doc_path)
--- EconomyAgent.py
+++ EconomyAgent.py
@@ -1,62 +1,54 @@
 import os
 from crewai import Agent, Task, Crew, Process
 from crewai_tools import SerperDevTool
-from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from datetime import date
+from datetime import date, datetime
+from DocxGenerator import generateDocx
+from tools.BCRATool import BCRATool
 
 load_dotenv()
 day = date.today()
+full_day = datetime.today()
 searchtool = SerperDevTool()
+bcra_tool = BCRATool()
 
 researcher = Agent(
-    role="Asistente de Investigación Senior",
-    goal=f"Buscar las últimas noticias sobre la economía y el estado del mercado de Argentina para el dia {day}.",
-    backstory="""Eres un dibulgador de informacion altamente capacitado, preciso y útil para entender los cambios económicos en la economía de Argentina.
-    Tu especialidad es buscar en Google eventos y cambios en la industria, política y el mercado que pueden llevar a consecuencias económicas.
-    Eres bueno entendiendo los cambios del mercado y sus implicaciones en la economía. 
-    Ejemplo de informacion que buscas es: cotizacion del dolar hoy, suba de acciones de mercado libre, nueva empresa unicornio argentina, etc.
-    """,
-    verbose=False,
+    role="Asistente de Investigación Profesional",
+    goal=f"Buscar y encontrar cambios específicos y actuales en la economía argentina para el día {day}, enfocándose en datos económicos concretos sin sesgos ideológicos.",
+    backstory="""Especialista en la recopilación de información económica actualizada, con énfasis en datos objetivos y relevantes para análisis económicos y financieros.
+    Utiliza la bcra_tool para acceder a indices del banco central de argentina.""",
+    tools=[searchtool, bcra_tool],
+    verbose=True,
     allow_delegation=False,
-    tools=[searchtool],
-    llm=ChatOpenAI(model_name="gpt-4-0125-preview", temperature=0.2)
 )
+
 writer = Agent(
     role="Escritor Profesional de Artículos Cortos",
-    goal=f"Resumir las últimas noticias en la economía de Argentina para el dia {day}.",
-    backstory="""Eres un excelente escritor de artículos de economía. Simplificas conceptos complejos en items de 2 o 3 parrafos o frases cortas.
-    Informas de manera clara pero preofesionalmente tus noticias.
-    """,
-    verbose=False,
+    goal=f"Resumir las últimas noticias en la economía de Argentina para el día {day}, manteniendo un enfoque neutral y basado en datos.",
+    backstory="""Experto en la redacción de artículos económicos claros y objetivos, utilizando información precisa y actualizada para informar al público.""",
+    verbose=True,
     allow_delegation=True,
     tools=[searchtool],
-    llm=ChatOpenAI(model_name="gpt-4-0125-preview", temperature=0.7)
 )
+
 research_task = Task(
-    description=f"""Realizar un análisis comprensivo enfocándose en los varios factores que influyen en la economía de Argentina para el dia {day}. 
-    Este análisis debería cubrir variaciones del mercado, decisiones políticas, cambios en la industria y el impacto de nuevas tecnologías.
-    Es esencial remarcar las tendencias del mercado asi como las tendencias de la tecnologia en el pais.
-    Ejemplo de informacion que buscas es: cotizacion del dolar hoy, suba de acciones de mercado libre, nueva empresa unicornio argentina, nueva tecnologia llega a argentina, nueva empresa importante sale a la bolsa, etc.""",
-    expected_output="""Se debera presentar un informe detallado para cada noticia o informacion a documentar.""",
-    agent= researcher
+    description=f"""Buscar datos económicos específicos y actualizados de Argentina para el día {day}, incluyendo cotizaciones de divisas, índices bursátiles, y movimientos de acciones relevantes.""",
+    agent=researcher,
+    expected_output="""Informe detallado con datos económicos concretos de la última semana.""",
 )
+
 write_task = Task(
-    description="""Utilizando los conocimientos proporcionados, escribir un artículo que destaque las noticias más significativas del mercado. 
-    Esto debe ser significativo para toma de decisiones, de inversiones o para entender las tendencias actuales tanto en mercado como tecnologia.""",
-    expected_output="""Publicación completa en el blog con al menos 8 noticias, cambios o cosas que saber sobre el mercado. 
-    La información debe ser compilada en una sección de 'Anexo' al final del documento, detallando el impacto específico de cada item en la economía de Argentina.
-    Cada sección del análisis (variaciones del mercado, decisiones políticas, cambios en la industria, nuevas tecnologías) debe estar definida con un titulo, su texto y su nivel de importancia del 1 al 10 en relacion a los demas puntos del resumen.
-    Toda esta informacion debe ser sobre temas recientes (no mas de una semana de antiguedad).
-    """,
-    agent= writer
+    description="""Escribir un artículo resumiendo las noticias económicas más relevantes de la semana, con un enfoque neutral y basado en datos. En español""",
+    agent=writer,
+    expected_output="""Artículo completo con resúmenes claros y objetivos de las noticias económicas más importantes de la última semana.""",
 )
+
 crew = Crew(
-    agents = [researcher, writer],
-    tasks= [research_task, write_task],
-    verbose=False
+    agents=[researcher, writer],
+    tasks=[research_task, write_task],
+    process=Process.sequential,
+    verbose=True,
+    output_log_file=f"Logs{full_day}.txt",
 )
 result = crew.kickoff()
-
-def getResult():
-    return result
+generateDocx(result)
